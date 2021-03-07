98fe3155d06aea8745fc67628e5a257a145a6d75
sunil <45433248+sunil7634@users.noreply.github.com>
2021-03-07 16:15:41 -0600

    Update cache_matrix.R

--- cache_matrix.R
+++ cache_matrix.R
@@ -21,3 +21,16 @@ cacheSolve<-function(x,...){
   x$setInverse(inv)
   inv
 }
+
+#solution
+# pmatrix<-makeCacheMatrix(matrix(1:4, nrow=2,ncol=2))
+# pmatrix$get()
+#     [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
+# pmatrix$getInverse()
+# NULL
+# cacheSolve(pmatrix)
+#     [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
